19e38de235f16c9a490d7e2d5a99e53caa5ad9d0
Nikhil Yarra <nikhilyarra01@gmail.com>
2025-06-13 06:18:24 -0400

    unsupported app router export const PATCH = async (     req: Request,     { params }: { params: { jobId: string } } ) => { ... }

--- app/api/jobs/[jobId]/unpublish/route.ts
+++ app/api/jobs/[jobId]/unpublish/route.ts
@@ -1,50 +1,44 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const PATCH = async (
-    req: Request,
-    {params}: {params :{jobId : string}}
-)=>{
-    try{
-        const {userId} =  await auth();
-        const {jobId} = params
+export const PATCH = async (req: NextRequest) => {
+  try {
+    const { userId } = await auth();
 
-        if(!userId){
-            return new NextResponse("Unauthorized", {status: 401});
-        }
+    const jobId = req.nextUrl.pathname.split("/").at(-2); // assumes route: /jobs/[jobId]/unpublish
 
-        if(!jobId){
-            return new NextResponse("ID is missing", {status: 404});
-        }
-
-        const job = await db.job.findUnique({
-            where : {
-                id: jobId,
-                userId
-            }
-        })
-
-        if(!job){
-            return new NextResponse("Job not found", {status: 404})
-        }
-
-
-        const publishJob = await db.job.update({
-            where :{
-                id: jobId,
-            },
-            data: {
-                isPublished: false,
-            }
-        })
-
-        return NextResponse.json(publishJob)
-
-    }catch(error){
-        console.log(`[JOB_PUBLISH_PATCH] : ${error}`);
-        return new NextResponse("Internal error", {status:500})
-
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
 
-}
+    if (!jobId) {
+      return new NextResponse("ID is missing", { status: 404 });
+    }
+
+    const job = await db.job.findUnique({
+      where: {
+        id: jobId,
+        userId,
+      },
+    });
+
+    if (!job) {
+      return new NextResponse("Job not found", { status: 404 });
+    }
+
+    const unpublishedJob = await db.job.update({
+      where: {
+        id: jobId,
+      },
+      data: {
+        isPublished: false,
+      },
+    });
+
+    return NextResponse.json(unpublishedJob);
+  } catch (error) {
+    console.log(`[JOB_UNPUBLISH_PATCH] : ${error}`);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+};
